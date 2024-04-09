7e6572d9ab21e791eac0676fae386ee0f3c79b80
IrynaVoronina <ira.voronina2004@gmail.com>
2024-04-09 10:18:15 +0300

    created ResultConsolePrinter

---
+++ DataManager/src/main/java/org/example/utilities/ResultConsolePrinter.java
@@ -0,0 +1,21 @@
+package org.example.utilities;
+
+public class ResultConsolePrinter {
+    public void printResultToConsole(double[][] result) {
+        System.out.println("\n ==================== MA ==================== \n");
+        for (double[] row : result) {
+            for (double element : row) {
+                System.out.print(element + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printResultToConsole(double[] result) {
+        System.out.println("\n\n ==================== E ==================== \n");
+        for (double element : result) {
+            System.out.print(element + "\t");
+        }
+        System.out.println();
+    }
+}
